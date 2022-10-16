c588b432423e69bf555d956568739e60310f9d12
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-10-16 18:25:49 -0400

    add post method

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000
 
 let videos = [
     {
-        id: 1,
+        id: new Date().toISOString(),
         title: "About JS - 01",
         author: "IT-INCUBATOR.EU",
         canBeDownloaded: true,
@@ -26,10 +26,48 @@ app.delete( '/testing/all-data', (req: Request, res:Response) =>{
     videos.length = 0
     res.send(204)
 })
-
-
 app.get('/videos', (req: Request, res: Response) =>{
     res.send(videos)
+})
+app.post('/videos', (req:Request, res: Response) =>{
+    let title = req.body.title
+    if(!title || typeof title !== "string" || !title.trim() || title.length > 40){
+        res.status(400).send({
+            errorsMessages: [
+                {
+                "message": "Incorrect title",
+                "field": "title"
+                }
+            ],
+            resultCode: 1
+        })
+    }
+    let author =req.body.author
+    if(!author || typeof author !== "string" || !title.trim() || author.length > 20){
+        res.status(400).send({
+            errorsMessages: [
+                {
+                    "message": "Incorrect title",
+                    "field": "title, author"
+                }
+            ],
+            resultCode: 1
+        })
+    }
+    const newVideo = {
+        id: new Date().toISOString(),
+        title: req.body.title,
+        author: req.body.author,
+        canBeDownloaded: true,
+        minAgeRestriction: null,
+        createdAt: "2022-10-16T15:51:40.667Z",
+        publicationDate: "2022-10-16T15:51:40.667Z",
+        availableResolutions: [
+            "P144"
+        ]
+    }
+    videos.push(newVideo)
+    res.status(201).send(newVideo)
 })
 
 
